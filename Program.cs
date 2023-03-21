24a3403fa9f514b990cc24f356db6ad12496a4ec
DevDaniloFerrari <consultor.ferrari@leomadeiras.com.br>
2023-03-21 08:55:25 -0300

    implementing menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("What do you wanna do?");
+    Console.WriteLine("1 - Open file");
+    Console.WriteLine("2 - Create a new file");
+    Console.WriteLine("0 - Exit");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default: Menu(); break;
+    }
+
+}
+
+void Open()
+{
+
+}
+
+void Edit(){
+
+}
+
+Menu();
